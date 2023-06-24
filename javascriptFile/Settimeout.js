2cdcd4b2840dfd9345fdf829b479d924e2aedd03
BASU <117992350+blacksword1123@users.noreply.github.com>
2023-06-24 11:57:39 +0700

    Delete javascriptFile directory

--- javascriptFile/Settimeout.js
+++
@@ -1,4 +0,0 @@
-setTimeout(() => {
-    console.log("This message will display");
-    console.log("After 1 second");
-}, 1000);
--- javascriptFile/define_property.js
+++
@@ -1,7 +0,0 @@
-let song = {};
-
-song.name = "Good Guys";
-song.artist = "LANY";
-song.year = 2020;
-
-console.log(song);
