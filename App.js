a683397db965f90411ce0eddad5047e66880dfea
BASU <117992350+blacksword1123@users.noreply.github.com>
2023-06-24 12:20:33 +0700

    CatJs

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Cat from './components/Cat';
+
+export default function App() {
+  return (
+    <View>
+      <Cat/>
+    </View>
+  );
+}
+
