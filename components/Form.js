0d40fa0875daab7d188fbe16951024f0235cd160
BASU <117992350+blacksword1123@users.noreply.github.com>
2023-07-22 10:56:29 +0700

    Attendance[22-July-2023]

---
+++ components/Form.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import React, { useState } from "react";
+
+const Form = () => {
+  const [name, setName] = useState("Taylor");
+  const [age, setAge] = useState(42);
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Enter Your Name"
+        style={styles.textInputStyle}
+        value={name}
+        onChangeText={(value) => {
+          setName(value);
+        }}
+      />
+      <Text>{"\n\n"}</Text>
+      <Button
+        title="INCREMENT AGE"
+        onPress={() => setAge(age+1)}
+      />
+      <Text>{"\n\n"}</Text>
+      <Text>Hello,{name}. You are {age}</Text>
+    </View>
+  );
+};
+
+export default Form;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 35,
+  },
+  textInputStyle: {
+    width: "100%",
+    height: 40,
+    paddingHorizontal: 5,
+    borderWidth: 0.5,
+    marginTop: 15,
+  },
+});
