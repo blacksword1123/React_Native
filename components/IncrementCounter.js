4e19c5ef643ec8dbf37ccb2eb8d7f57d0c77ebc6
BASU <117992350+blacksword1123@users.noreply.github.com>
2023-07-22 11:01:31 +0700

    Attendance[22-July-2023]

---
+++ components/IncrementCounter.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+
+const IncreamentCounter = () => {
+  const [age, setAge] = useState(23);
+
+  const increment = () => {
+    setAge((a) => a + 1);
+  };
+
+  return (
+    <View>
+      <Text style={{ fontSize: 24, fontStyle: "bold" }}>Your age: {age}</Text>
+      <Text>{"\n\n"}</Text>
+      <Button title="+3" onPress={() => setAge(age + 3)} />
+      <Text>{"\n\n"}</Text>
+      <Button
+        title="+1"
+        onPress={(age) => {
+          increment(age);
+        }}
+      />
+    </View>
+  );
+};
+
+export default IncreamentCounter;
+
+const styles = StyleSheet.create({});
