495033d4af58ff0c80b396ccde77d3294bacebcc
BASU <117992350+blacksword1123@users.noreply.github.com>
2023-08-05 09:47:58 +0700

    Attendance[05-August-2023]

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import TouchableExample from './components/TouchableExample'
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import TouchablePractice from "./components/TouchablePractice";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <TouchableExample/>
+    <View style={{flex:1}}>
+      <TouchablePractice />
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  }
-})
+const styles = StyleSheet.create({});
